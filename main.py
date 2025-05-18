6519cb561c71e0abc64335cc84bf91f1cfd12051
Nitin Singh <nitintwt27@gmail.com>
2025-05-18 16:57:38 +0530

    crud operation using fast api

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+
+app = FastAPI()
+
+#data types 
+class Tea(BaseModel):
+  id:int
+  name:str
+  origin:str
+
+# teas is a list , which includes Tea datatypes
+teas:List[Tea]=[]
+
+@app.get("/")
+def read_root():
+  return {"message":"welcome to tea house"}
+
+@app.get("/teas")
+def get_teas():
+  return teas
+
+@app.post("/teas")
+def add_tea(tea:Tea):
+  teas.append(tea)
+  return tea
+
+@app.put("/teas/${tea_id}")
+def update_tea(tea_id:int , updated_tea:Tea):
+  for index, tea in enumerate(teas):
+    if tea.id==tea_id:
+      teas[index]=update_tea
+      return update_tea
+  return {"error":"Tea not found"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id:int):
+  for index , tea in enumerate(teas):
+    if tea.id==tea_id:
+      teas.pop(index)
+      return{"message":"Tea deleted successfully"} 
+  return {"error":"Tea not found"}
